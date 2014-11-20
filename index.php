f36440b627b6f0657486aeca90a1e74eaaf2b1b6
Diego Verástegui <diegoverastegui@gmail.com>
2014-11-20 12:16:44 -0500

    dffdfdf

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Diseño y Desarrollo Web, Marketing Digital, Branding.</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+
+    <!-- Animate CSS -->
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:500,300,700,400' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-play-circle"></i>  <span class="light">EMPRESA</span><span class="text-yellow">WEB</span>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">ACERCA</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#servicios">Servicios</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#equipo">Equipo</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 animated flipInX">
+                        <h1 class="brand-heading">EMPRESA<span class="text-yellow">WEB</span></h1>
+                        <p class="intro-text">Diseñamos y tomamos café.</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Introducción -->
+    <section id="download" class="content-section text-center">
+        <div class="introduccion-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Introducción de la web</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a href="#" class="action-button shadow animate yellow">Algún enlace</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Servicios Section -->
+    <section id="servicios" class="content-section branding descripcion-servicios">
+        <div class="servicios-web-section">
+        <div class="container servicios-section">
+            <div class="col-sm-5 col-md-5">
+                <h2>Diseño y Desarrollo Web</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#" class="action-button shadow animate yellow">Ver más</a>
+            </div>
+            <div class="col-sm-7 col-md-7 text-center">
+                <p>(Imagen)</p>
+            </div>
+        </div>
+    </div>
+            <div class="servicios-apps-section">
+        <div class="container servicios-section">
+        <div class="col-sm-7 col-md-7 text-center">
+            <p>(Imagen)</p>
+            </div>
+            <div class="col-sm-5 col-md-5">
+                <h2>Aplicaciones Móviles</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#" class="action-button shadow animate yellow">Ver más</a>
+            </div>
+        </div>
+        </div>
+        <div class="servicios-marketing-section">
+        <div class="container servicios-section">
+            <div class="col-sm-5 col-md-5">
+                <h2>Marketing Digital</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#" class="action-button shadow animate yellow">Ver más</a>
+            </div>
+            <div class="col-sm-7 col-md-7 text-center">
+                <p>(Imagen)</p>
+            </div>
+        </div>
+        </div>
+        <div class="servicios-branding-section">
+        <div class="container servicios-section">
+        <div class="col-sm-7 col-md-7 text-center">
+            <p>(Imagen)</p>
+            </div>
+            <div class="col-sm-5 col-md-5">
+                <h2>Identidad Gráfica</h2>
+                <p>We take care of everything about an advertising campaign: branding and corporate identity, brochures, catalogs and posters. With a very careful eye on the consistency of the brand across all media channels.</p>
+                <a href="#" class="action-button shadow animate yellow">Ver más</a>
+            </div>
+        </div>
+
+
+        </div>
+    </section>
+
+    <!-- Twitter Section -->
+    <section id="download" class="content-section">
+        <div class="twitter-section">
+            <div class="container">
+                <div class="col-lg-12 text-center">
+                    <h2>Twitter Section</h2>
+                    <p> We're A Creative Web Design Agency Making Great Happen Daily From Long Island, New York.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Equipo Section -->
+    <section id="download" class="content-section">
+        <div class="equipo-section">
+            <div class="container">
+                <div class="col-lg-5 col-lg-offset-8">
+                    <h2>GREAT WORK REQUIRES A GREAT TEAM</h2>
+                    <p>Learn more about the people who make great happen every day.</p>
+                    <a href="http://startbootstrap.com/template-overviews/grayscale/" class="action-button shadow animate yellow">Equipo</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Café Section -->
+    <section id="cafes" class="content-section ">
+        <div class="cafes-section">
+            <div class="container">
+                <div class="col-lg-3 text-center">
+                   <div class="contadores">
+                        <div class="cont_numero">48</div>
+                        <div class="cont_descripcion">Tazas de Café</div>
+                   </div>
+                </div>
+        <div class="col-lg-3 text-center">
+                   <div class="contadores">
+                        <div class="cont_numero">1038</div>
+                        <div class="cont_descripcion">Líneas de código</div>
+                   </div>
+                </div>
+        <div class="col-lg-3 text-center">
+                   <div class="contadores">
+                        <div class="cont_numero">682</div>
+                        <div class="cont_descripcion">Horas de trabajo</div>
+                   </div>
+                </div>
+        <div class="col-lg-3 text-center">
+                   <div class="contadores">
+                        <div class="cont_numero">28</div>
+                        <div class="cont_descripcion">Proyectos terminados</div>
+                   </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+
+
+    <!-- Footer -->
+<?php include 'footer.php';?>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+    <script src="js/viewportchecker.js"></script> 
+    <script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery('.post').addClass("hidden").viewportChecker({
+            classToAdd: 'visible animated fadeIn', // Class to add to the elements when they are visible
+            offset: 100    
+       });   
+});            
+</script>
+
+
+</body>
+
+</html>
